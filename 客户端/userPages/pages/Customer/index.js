9effa7e01a1d9566081c1e822bc93b60bedc33eb
DaiSiYang <2761162439@qq.com>
2024-12-03 14:52:32 +0800

    完成客服功能

--- 客户端/userPages/pages/Customer/index.js
+++ 客户端/userPages/pages/Customer/index.js
@@ -1,58 +1,50 @@
-// userPages/pages/Customer/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
-  },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+    messages: [],   // 消息列表
+    inputMessage: '',  // 用户输入
+    scrollTop: 0  // 控制滚动条位置
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
+  // 输入框内容变化时更新
+  onInput: function (e) {
+    this.setData({
+      inputMessage: e.detail.value
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
+  // 发送消息
+  sendMessage: function () {
+    const message = this.data.inputMessage.trim();
+    if (message) {
+      this.addMessage('用户', message);
+      this.setData({ inputMessage: '' });
 
+      // 模拟客服回复
+      const response = this.getResponse(message);
+      setTimeout(() => {
+        this.addMessage('客服', response);
+      }, 1000);  // 模拟客服回复延迟
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
+  // 向消息列表添加消息
+  addMessage: function (from, text) {
+    this.setData({
+      messages: [...this.data.messages, { from, text, id: Date.now() }],
+      scrollTop: this.data.scrollTop + 100 // 控制滚动条向下滚动
+    });
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
+  // 模拟客服回复
+  getResponse: function (message) {
+    const responses = {
+      '你认识徐昊吗':"我认识，他好像是一个舔狗，很出名",
+      '你好': '您好，欢迎咨询！有什么可以帮您的吗？',
+      '产品': '我们有多种产品，包括茶杯，茶具，茶叶等。',
+      '退货': '如果您需要退货，可以去阿富汗线下店退货',
+      '售后': '我们的售后服务电话是 119-119-119。'
+    };
 
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+    return responses[message] || '抱歉，我没有理解您的问题，请重新提问。';
   }
-})
+});
