3450a225881981910f2a8706eb7764775c29b4c3
DaiSiYang <2761162439@qq.com>
2024-12-12 20:11:14 +0800

    优化免费好ç毫免费好课ui设计

--- 客户端/couresPages/pages/free/free.js
+++ 客户端/couresPages/pages/free/free.js
@@ -12,23 +12,71 @@ Component({
    * 组件的初始数据
    */
   data: {
-    itemList:[
-    //   {text:"中国茶文化简史",id:1,day:"8月25日",price:"0.02",
-    //  content:"中国茶文化是中国制茶、饮茶的文化。中国是茶的故乡，中国人发现并利用茶，据说始于神农时代，少说也有4700多年了。直到现在，汉族还有民以茶代礼的风俗"},
-    //   {text:"茶经",id:2,day:"8月26日",price:"0.03",
-    //  content:"《茶经》是唐代‌陆羽创作的茶学专著，是中国乃至世界现存最早、最完整、最全面的茶叶专著，被誉为茶叶百科全书。全书分为上中下三卷，共十章节，系统地总结了唐代中期以前茶叶发展、生产、加工、品饮等方面的情形，并深入发掘饮茶的文化内涵，将饮茶从日常生活习惯提升到了艺术和审美的层次。"},
-    //   {text:"茶思想研讨",id:3,day:"8月27日",price:"0.04",
-    //  content:"茶虽简单，却蕴含无限韵味，提醒我们要珍视眼前的每一刻，活在当下，用心去体验生活中的点点滴滴，发现平凡中的美好，感恩生命赐予的一切"},
-    //   {text:"东西方艺术欣赏",id:4,day:"8月28日",price:"0.05",
-    //  content:"西方艺术家学习借鉴东方艺术的两个高潮是印象派和超现实主义，区别在于印象派关注东方艺术的符号和元素，超现实主义则更关注东方精神和艺术技法"},
-    //   {text:"泡茶技艺初级",id:5,day:"9月5日",price:"0.06",
-    //  content:"茶道，就是品赏茶的美感之道。茶道是一种烹茶饮茶的生活艺术，一种以茶修身的生活方式"},
-    //   {text:"初级实践",id:6,day:"9月6日",price:"0.07",
-    //  content:"茶的泡法 泡茶时应根据不同茶类的特点来调整水的温度、浸润的时间和茶叶的用量,这样茶的香味、色泽、滋味才会得以充分的发挥"},
-    //   {text:"中级实践",id:7,day:"9月12日",price:"0.08",
-    //  content:"泡茶时应根据不同茶类的特点来调整水的温度、浸润的时间和茶叶的用量"},
-    //   {text:"高级实践",id:8,day:"9月21日",price:"0.09",
-    //  content:"泡茶时应根据不同茶类的特点来调整水的温度、浸润的时间和茶叶的用量"},
+    courseList: [
+      {
+        id: 1,
+        title: "中国茶文化简史",
+        imageUrl: "https://mmbiz.qpic.cn/mmbiz_jpg/5eODvZ3nNSwH4aZ2NvyiahB9vKxgV5mhGvaMjMCBSPiciaU7DHd1gpDiamemvS1YaGhVMPQ8K5VKJRXJNWzVlOxAOQ/640?wx_fmt=jpeg", // 请确保图片路径正确
+        date: "8月25日",
+        views: 2451,
+        description: "中国茶文化是中国制茶、饮茶的文化。中国是茶的故乡，中国人发现并利用茶，据说始于神农时代，少说也有4700多年了。"
+      },
+      {
+        id: 2,
+        title: "茶经",
+        imageUrl: "https://gw.alicdn.com/imgextra/O1CN01kngmoQ2LY22g1VJ3L_!!3937219703-0-C2M.jpg_460x460q90.jpg_.webp",
+        date: "8月26日",
+        views: 1832,
+        description: "《茶经》是唐代陆羽创作的茶学专著，是中国乃至世界现存最早、最完整、最全面的茶叶专著，被誉为茶叶百科全书。"
+      },
+      {
+        id: 3,
+        title: "茶思想研讨",
+        imageUrl: "https://img.alicdn.com/img/i2/40543961/O1CN01QEYaAq1f8BiZiDv3O_!!0-saturn_solar.jpg_460x460q90.jpg_.webp",
+        date: "8月27日",
+        views: 1654,
+        description: "茶虽简单，却蕴含无限韵味，提醒我们要珍视眼前的每一刻，活在当下，用心去体验生活中的点点滴滴。"
+      },
+      {
+        id: 4,
+        title: "东西方艺术欣赏",
+        imageUrl: "https://img.alicdn.com/img/i4/110487193/O1CN01ghCH18230RnEqjXDf_!!0-saturn_solar.jpg_460x460q90.jpg_.webp",
+        date: "8月28日",
+        views: 1245,
+        description: "西方艺术家学习借鉴东方艺术的两个高潮是印象派和超现实主义，区别在于印象派关注东方艺术的符号和元素。"
+      },
+      {
+        id: 5,
+        title: "泡茶技艺初级",
+        imageUrl: "https://img.alicdn.com/img/i1/5273923003/O1CN01lzJhHx1Y3QL3eUFBU_!!2-saturn_solar.png_460x460q90.jpg_.webp",
+        date: "9月5日",
+        views: 1102,
+        description: "茶道，就是品赏茶的美感之道。茶道是一种烹茶饮茶的生活艺术，一种以茶修身的生活方式。"
+      },
+      {
+        id: 6,
+        title: "初级实践",
+        imageUrl: "https://g-search1.alicdn.com/img/bao/uploaded/i4/i1/747396235/O1CN01pXN5xi1vvfxJaJL1c_!!0-item_pic.jpg_460x460q90.jpg_.webp",
+        date: "9月6日",
+        views: 986,
+        description: "茶的泡法 泡茶时应根据不同茶类的特点来调整水的温度、浸润的时间和茶叶的用量,这样茶的香味才会充分发挥。"
+      },
+      {
+        id: 7,
+        title: "中级实践",
+        imageUrl: "https://img.alicdn.com/img/i3/110683765/O1CN01yfjDh11dgPzFjYoJt_!!0-saturn_solar.jpg_460x460q90.jpg_.webp",
+        date: "9月12日",
+        views: 875,
+        description: "进阶茶艺技法，掌握不同茶类的冲泡要领，了解茶具的选择与搭配，提升泡茶技艺。"
+      },
+      {
+        id: 8,
+        title: "高级实践",
+        imageUrl: "https://img.alicdn.com/img/i3/40543961/O1CN01qKRkCb1f8BhSlEOu9_!!0-saturn_solar.jpg_460x460q90.jpg_.webp",
+        date: "9月21日",
+        views: 764,
+        description: "深入学习茶道礼仪，掌握茶席布置技巧，领悟茶道精神，实现茶艺的更高境界。"
+      }
     ]
   },
 
@@ -36,26 +84,20 @@ Component({
    * 组件的方法列表
    */
   methods: {
-      getItemList(){
-        wx.request({
-          url: "http://127.0.0.1:8080/api/course/Free",
-          method:"GET",
-          success:(res)=>{
-            this.setData({
-              itemList:res.data
-            })
-            console.log(this.itemList);
-          }
-        })
-      },
-      onMore(){
-        wx.showToast({
-          title: '没有更多了',
-          icon:"none"
-        })
-      }
-  },
-  attached(){
-    this.getItemList()
+    // 搜索课程
+    onSearch(e) {
+      const searchValue = e.detail.value;
+      // 这里可以添加搜索逻辑
+      console.log('搜索:', searchValue);
+    },
+
+    // 点击课程卡片
+    onCourseTap(e) {
+      const courseId = e.currentTarget.dataset.id;
+      // 这里可以添加课程点击处理逻辑
+      wx.navigateTo({
+        url: `/pages/courseDetail/courseDetail?id=${courseId}`
+      });
+    }
   }
-})
+});
