b1d2b174ed0a8bf9edad23304b72e3acfea88350
DaiSiYang <2761162439@qq.com>
2024-12-12 19:06:59 +0800

    优化主页轮播图

--- 客户端/components/course/banner/index.js
+++ 客户端/components/course/banner/index.js
@@ -1,37 +1,48 @@
-// components/course/banner/index.js
-Component({
-
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
-   */
+Page({
   data: {
-      bannerList:[]
+    currentIndex: 0,
+    swiperList: [
+      {
+        id: 1,
+        imageUrl: 'https://img2.baidu.com/it/u=481590814,1241413913&fm=253&fmt=auto&app=120&f=JPEG?w=712&h=474',
+        title: '清香雅韵',
+        desc: '品味人生，品味茶香'
+      },
+      {
+        id: 2,
+        imageUrl: 'https://img0.baidu.com/it/u=813936022,738342146&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500',
+        title: '茗茶飘香',
+        desc: '一杯好茶，沁人心脾'
+      },
+      {
+        id: 3,
+        imageUrl: 'https://img2.baidu.com/it/u=2571529661,3489841314&fm=253&fmt=auto&app=138&f=JPEG?w=750&h=500',
+        title: '茶艺之美',
+        desc: '匠心独运，韵味悠长'
+      },
+      {
+        id: 4,
+        imageUrl: 'https://img0.baidu.com/it/u=2258170876,2900753318&fm=253&fmt=auto&app=138&f=JPEG?w=700&h=400',
+        title: '禅茶一味',
+        desc: '清心静气，品味生活'
+      },
+      {
+        id: 5,
+        imageUrl: 'https://img2.baidu.com/it/u=553827459,2332021544&fm=253&fmt=auto&app=138&f=JPEG?w=751&h=500',
+        title: '茶道之境',
+        desc: '一期一会，茶香永驻'
+      }
+    ]
   },
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-      getBannerList(){
-        wx.request({
-          url: 'http://127.0.0.1:8080/api/course/banner',
-          method:"GET",
-          success: (result) => {
-            this.setData({
-              bannerList : result.data
-            })
-          }
-        })
-      }
+  handleSwiperClick(e) {
+    const id = e.currentTarget.dataset.id;
+    console.log('点击了轮播图，ID:', id);
   },
-  attached(){
-    this.getBannerList()
+
+  handleSwiperChange(e) {
+    this.setData({
+      currentIndex: e.detail.current
+    });
   }
-})
+}) 
--- 客户端/components/home/banner/index.js
+++ 客户端/components/home/banner/index.js
@@ -1,30 +1,48 @@
-// components/home/banner/index.js
-Component({
-
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
+Page({
+  data: {
+    currentIndex: 0,
+    swiperList: [
+      {
+        id: 1,
+        imageUrl: 'https://img2.baidu.com/it/u=481590814,1241413913&fm=253&fmt=auto&app=120&f=JPEG?w=712&h=474',
+        title: '清香雅韵',
+        desc: '品味人生，品味茶香'
+      },
+      {
+        id: 2,
+        imageUrl: 'https://img0.baidu.com/it/u=813936022,738342146&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500',
+        title: '茗茶飘香',
+        desc: '一杯好茶，沁人心脾'
+      },
+      {
+        id: 3,
+        imageUrl: 'https://img2.baidu.com/it/u=2571529661,3489841314&fm=253&fmt=auto&app=138&f=JPEG?w=750&h=500',
+        title: '茶艺之美',
+        desc: '匠心独运，韵味悠长'
+      },
+      {
+        id: 4,
+        imageUrl: 'https://img0.baidu.com/it/u=2258170876,2900753318&fm=253&fmt=auto&app=138&f=JPEG?w=700&h=400',
+        title: '禅茶一味',
+        desc: '清心静气，品味生活'
+      },
+      {
+        id: 5,
+        imageUrl: 'https://img2.baidu.com/it/u=553827459,2332021544&fm=253&fmt=auto&app=138&f=JPEG?w=751&h=500',
+        title: '茶道之境',
+        desc: '一期一会，茶香永驻'
+      }
+    ]
   },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
-    imageList:[
-      {url:"https://img2.baidu.com/it/u=481590814,1241413913&fm=253&fmt=auto&app=120&f=JPEG?w=712&h=474",id:1},
-      {url:"https://img0.baidu.com/it/u=813936022,738342146&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500",id:2},
-      {url:"https://img2.baidu.com/it/u=2571529661,3489841314&fm=253&fmt=auto&app=138&f=JPEG?w=750&h=500",id:3},
-      {url:"https://img0.baidu.com/it/u=2258170876,2900753318&fm=253&fmt=auto&app=138&f=JPEG?w=700&h=400",id:4},
-      {url:"https://img2.baidu.com/it/u=553827459,2332021544&fm=253&fmt=auto&app=138&f=JPEG?w=751&h=500",id:5},
-    ]
-    },
+  handleSwiperClick(e) {
+    const id = e.currentTarget.dataset.id;
+    console.log('点击了轮播图，ID:', id);
+  },
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
+  handleSwiperChange(e) {
+    this.setData({
+      currentIndex: e.detail.current
+    });
   }
-})
+}) 
